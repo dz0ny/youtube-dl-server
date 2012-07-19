36c98de80abfa535c6cbabbbcf9065ca81f7c1a5
Janez Troha <janez.troha@gmail.com>
2012-07-19 16:54:54 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ GITHUB=https://github.com/dz0ny/youtube-dl-server
 rm -rf ~/youtube-dl-server
 mkdir ~/youtube-dl-server
 cd ~/youtube-dl-server
-wget --no-check-certificate $GITHUB/tarball/master -O ./youtube-dl-server.tar.gz
+curl -L $GITHUB/tarball/master -O ./youtube-dl-server.tar.gz
 tar -zxvf ./youtube-dl-server.tar.gz --strip 1
 rm ./youtube-dl-server.tar.gz
 rm ./install.sh
